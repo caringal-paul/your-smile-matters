4ed3ba4ab62318df794ec99ed476c9f0de3535a8
Daniel Caringal <daniel.caringal@opsolutions.biz>
2026-05-11 11:22:01 +0800

    YSM-17 | [Checkpoint] Notification

--- src/ami/features/auth/utils/types/auth-response.ami.types.ts
+++ src/ami/features/auth/utils/types/auth-response.ami.types.ts
@@ -1,29 +1,29 @@
 import { UserModel } from "@/core/models/user.model";
 
 type RoleResponse = {
-	_id: string;
-	name: string;
-	description?: string;
-	permissions: string[];
+  _id: string;
+  name: string;
+  description?: string;
+  permissions: string[];
 };
 
 type UserAuthResponse = Pick<
-	UserModel,
-	| "_id"
-	| "email"
-	| "username"
-	| "first_name"
-	| "last_name"
-	| "mobile_number"
-	| "role_id"
+  UserModel,
+  | "_id"
+  | "email"
+  | "username"
+  | "first_name"
+  | "last_name"
+  | "mobile_number"
+  | "role_id"
 > & {
-	role_and_permissions: RoleResponse | null;
-	is_photographer?: boolean;
+  role_and_permissions: RoleResponse | null;
+  is_photographer?: boolean;
 };
 
 export type LoginResponseAmi = {
-	user: UserAuthResponse;
-	access_token: string;
-	refresh_token: string;
-	expires_in: string;
+  user: UserAuthResponse;
+  access_token: string;
+  refresh_token: string;
+  expires_in: string;
 };
--- src/ami/shared/components/header/PageHeader.tsx
+++ src/ami/shared/components/header/PageHeader.tsx
@@ -1,111 +1,113 @@
 import { useState } from "react";
 import { Label } from "../../../../core/components/base/label";
 import {
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
 } from "../../../../core/components/base/popover";
 import { useNavigate } from "react-router-dom";
 import { useGetCurrentUserLoggedInQuery } from "@/ami/features/account-settings/queries/getCurrentUserLoggedIn.ami.query";
 import {
-	Avatar,
-	AvatarFallback,
-	AvatarImage,
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
 } from "@/core/components/base/avatar";
 import { getInitials } from "@/core/helpers/getInitials";
 import { useLogoutUserMutation } from "@/ami/features/auth/queries/logoutAmiUser.mutation";
 import { useCurrentAmiUser } from "@/ami/store/useCurrentAmiUser";
+import { NotificationBell } from "@/core/components/custom/NotificationBell";
 
 type PageHeaderProps = { pageTitle: string };
 
 const PageHeader = ({ pageTitle }: PageHeaderProps) => {
-	const navigate = useNavigate();
-	const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const navigate = useNavigate();
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
 
-	const { data: currUserLoggedIn, isLoading: isUserDataFetching } =
-		useGetCurrentUserLoggedInQuery();
+  const { data: currUserLoggedIn, isLoading: isUserDataFetching } =
+    useGetCurrentUserLoggedInQuery();
 
-	const clearCurrentUser = useCurrentAmiUser((state) => state.clearCurrentUser);
-	const currentUser = useCurrentAmiUser((state) => state.currentUser);
+  const clearCurrentUser = useCurrentAmiUser((state) => state.clearCurrentUser);
+  const currentUser = useCurrentAmiUser((state) => state.currentUser);
 
-	const { mutateAsync: logout } = useLogoutUserMutation();
+  const { mutateAsync: logout } = useLogoutUserMutation();
 
-	return (
-		<div className="sticky z-10 top-20 xl:top-0">
-			<div className="flex items-center justify-between w-full px-4 py-2 rounded-md h-fit xl:py-3 xl:px-8 bg-accent">
-				<Label className="text-sm font-semibold md:text-base lg:text-lg xl:text-xl">
-					{pageTitle}
-				</Label>
+  return (
+    <div className="sticky z-10 top-20 xl:top-0">
+      <div className="flex items-center justify-between w-full px-4 py-2 rounded-md h-fit xl:py-3 xl:px-8 bg-accent">
+        <Label className="text-sm font-semibold md:text-base lg:text-lg xl:text-xl">
+          {pageTitle}
+        </Label>
 
-				<div className="flex gap-2 items-center">
-					<Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
-						<PopoverTrigger asChild>
-							<button
-								onClick={() => setIsPopoverOpen((prev) => !prev)}
-								className="size-8 bg-cover rounded-full"
-								aria-label="Profile"
-							>
-								<Avatar className="size-8 border-2 border-gray-300">
-									<AvatarImage
-										src={currUserLoggedIn?.profile_image || ""}
-										alt="@my-profile"
-										className="object-cover"
-									/>
-									<AvatarFallback>
-										{getInitials(
-											`${currUserLoggedIn?.first_name} ${currUserLoggedIn?.last_name}`
-										)}
-									</AvatarFallback>
-								</Avatar>
-							</button>
-						</PopoverTrigger>
+        <div className="flex gap-2 items-center">
+          <NotificationBell />
+          {/* ASD */}
+          <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
+            <PopoverTrigger asChild>
+              <button
+                onClick={() => setIsPopoverOpen((prev) => !prev)}
+                className="size-8 bg-cover rounded-full"
+                aria-label="Profile"
+              >
+                <Avatar className="size-8 border-2 border-gray-300">
+                  <AvatarImage
+                    src={currUserLoggedIn?.profile_image || ""}
+                    alt="@my-profile"
+                    className="object-cover"
+                  />
+                  <AvatarFallback>
+                    {getInitials(
+                      `${currUserLoggedIn?.first_name} ${currUserLoggedIn?.last_name}`,
+                    )}
+                  </AvatarFallback>
+                </Avatar>
+              </button>
+            </PopoverTrigger>
 
-						<PopoverContent
-							side="bottom"
-							align="start"
-							className="z-50 flex flex-col justify-start gap-1 p-2 mt-2 mr-8 bg-white w-fit"
-						>
-							<button
-								className="w-full p-2 text-xs bg-transparent hover:bg-accent text-foreground text-start"
-								onClick={() => {
-									if (currentUser?.is_photographer) {
-										navigate(
-											`/admin/ami/photographer-management/photographers/edit/photographer/${currentUser?._id}`
-										);
-									} else {
-										navigate("/admin/ami/account-settings");
-									}
-								}}
-							>
-								Account Settings
-							</button>
-							<button
-								className="w-full p-2 text-xs bg-transparent hover:bg-accent text-foreground text-start"
-								onClick={async () => {
-									await logout();
-									localStorage.removeItem("ami_access_token");
-									localStorage.removeItem("ami_refresh_token");
-									clearCurrentUser();
-									navigate("/admin/ami/auth");
-								}}
-							>
-								Logout
-							</button>
-						</PopoverContent>
-					</Popover>
-
-					<div className="flex flex-col gap-0 leading-none tracking-tight text-center">
-						<Label className="text-xs font-semibold text-foreground">
-							{currUserLoggedIn?.first_name} {currUserLoggedIn?.last_name}
-						</Label>
-						<Label className="text-[#74889E] text-[8px] font-light">
-							{currUserLoggedIn?.role_and_permissions?.name}
-						</Label>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+            <PopoverContent
+              side="bottom"
+              align="start"
+              className="z-50 flex flex-col justify-start gap-1 p-2 mt-2 mr-8 bg-white w-fit"
+            >
+              <button
+                className="w-full p-2 text-xs bg-transparent hover:bg-accent text-foreground text-start"
+                onClick={() => {
+                  if (currentUser?.is_photographer) {
+                    navigate(
+                      `/admin/ami/photographer-management/photographers/edit/photographer/${currentUser?._id}`,
+                    );
+                  } else {
+                    navigate("/admin/ami/account-settings");
+                  }
+                }}
+              >
+                Account Settings
+              </button>
+              <button
+                className="w-full p-2 text-xs bg-transparent hover:bg-accent text-foreground text-start"
+                onClick={async () => {
+                  await logout();
+                  localStorage.removeItem("ami_access_token");
+                  localStorage.removeItem("ami_refresh_token");
+                  clearCurrentUser();
+                  navigate("/admin/ami/auth");
+                }}
+              >
+                Logout
+              </button>
+            </PopoverContent>
+          </Popover>
+          <div className="flex flex-col gap-0 leading-none tracking-tight text-center">
+            <Label className="text-xs font-semibold text-foreground">
+              {currUserLoggedIn?.first_name} {currUserLoggedIn?.last_name}
+            </Label>
+            <Label className="text-[#74889E] text-[8px] font-light">
+              {currUserLoggedIn?.role_and_permissions?.name}
+            </Label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PageHeader;
